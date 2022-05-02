c4641c12dd13187dc89f88dce39f1e2834739457
nemeshma <Puls_200@mail.ru>
2022-05-02 17:23:54 +0300

    Создан пакет для моей первой программы

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sandbox;
+
+public class MyFirstProgram {
+
+  public static void main(String[] args) {
+    String somebody = "world";
+    System.out.println("Hello, " + somebody + "!");
+
+    System.out.println(2+2);
+    System.out.println(1.0/2);
+  }
+
+}
